a448fe5d3c1e8c2030e17018a39421f73f0a6e28
byd <boyuandeng01@gmail.com>
2025-08-14 19:44:47 -0400

    Update run_regression.py

--- demos/run_regression.py
+++ demos/run_regression.py
@@ -17,122 +17,103 @@ import numpy as np
 import tensorflow as tf
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-import socket
-
-import itertools
-
-from gpflow.likelihoods import Gaussian
-from gpflow.kernels import RBF, White
-from gpflow.training import AdamOptimizer
-
-import gpflow_monitor
-
 from scipy.cluster.vq import kmeans2
 from scipy.stats import norm
 from scipy.special import logsumexp
 
+from gpflow.likelihoods import Gaussian
+from gpflow.kernels import SquaredExponential, White
+import gpflow
+
+HERE = os.path.dirname(__file__)
+if HERE not in sys.path:
+    sys.path.insert(0, HERE)
+
 from doubly_stochastic_dgp.dgp import DGP
 from datasets import Datasets
-datasets = Datasets()
-
-results_path = '/vol/bitbucket/hrs13/tmp_results_{}/'.format(socket.gethostname())
-
-dataset_name = str(sys.argv[1])
-L = int(sys.argv[2])
-split = int(sys.argv[3])
 
 
-iterations = 10000
-log_every = 100
-minibatch_size = 10000
+def main():
+    dataset_name = str(sys.argv[1])
+    L = int(sys.argv[2])
+    split = int(sys.argv[3])
+
+    # Config
+    iterations = 10000
+    log_every = 100
+    minibatch_size = 10000
+
+    # Use float64 for GPflow numerical stability
+    gpflow.config.set_default_float(np.float64)
+
+    datasets = Datasets()
+    data = datasets.all_datasets[dataset_name].get_data(split=split)
+    X, Y, Xs, Ys, Y_std = [data[_] for _ in ['X', 'Y', 'Xs', 'Ys', 'Y_std']]
+
+    print('############################ {} L={} split={}'.format(dataset_name, L, split))
+    print('N: {}, D: {}, Ns: {}'.format(X.shape[0], X.shape[1], Xs.shape[0]))
+
+    # Inducing points via k-means
+    Z = kmeans2(X, 100, minit='points')[0]
+
+    # Kernels per layer
+    kernels = []
+    for _ in range(L):
+        k = SquaredExponential()
+        # Provide expected attribute for downstream init code
+        k.input_dim = X.shape[1]
+        kernels.append(k)
+    for i in range(max(0, L - 1)):
+        kernels[i] = kernels[i] + White(variance=2e-6)
+
+    mb = minibatch_size if X.shape[0] > minibatch_size else None
+    model = DGP(X, Y, Z, kernels, Gaussian(), num_samples=1, minibatch_size=mb)
+
+    # Start the inner layers almost deterministically
+    for layer in model.layers[:-1]:
+        layer.q_sqrt.assign(layer.q_sqrt * 1e-5)
+    # Set observation noise
+    model.likelihood.likelihood.variance.assign(0.05)
+
+    optimizer = tf.optimizers.Adam(0.01)
+
+    @tf.function
+    def train_step():
+        with tf.GradientTape() as tape:
+            # We maximize the ELBO, so minimize its negative
+            loss = -model.maximum_log_likelihood_objective()
+        grads = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        return -loss
+
+    def evaluate(S=100, batch_size=1000):
+        means, vars_ = [], []
+        num_batches = -(-len(Xs) // batch_size)
+        for mb_idx in range(num_batches):
+            start = mb_idx * batch_size
+            end = (mb_idx + 1) * batch_size
+            m, v = model.predict_y(Xs[start:end, :], S)
+            means.append(m.numpy())
+            vars_.append(v.numpy())
+        mean_SND = np.concatenate(means, axis=1)
+        var_SND = np.concatenate(vars_, axis=1)
+        mean_ND = np.average(mean_SND, axis=0)
+        rmse = np.average(Y_std * np.mean((Ys - mean_ND) ** 2.0) ** 0.5)
+        test_nll_ND = logsumexp(
+            norm.logpdf(Ys * Y_std, mean_SND * Y_std, np.sqrt(var_SND) * Y_std),
+            axis=0,
+            b=1 / float(S),
+        )
+        nll = np.average(test_nll_ND)
+        return rmse, nll
+
+    # Training loop
+    for step in range(1, iterations + 1):
+        elbo = train_step().numpy()
+        if step % log_every == 0:
+            rmse, nll = evaluate()
+            print(f"iter {step:6d} | ELBO: {elbo:.3f} | test_rmse: {rmse:.4f} | test_nlpp: {nll:.4f}")
 
 
-
-data = datasets.all_datasets[dataset_name].get_data(split=split)
-X, Y, Xs, Ys, Y_std = [data[_] for _ in ['X', 'Y', 'Xs', 'Ys', 'Y_std']]
-
-print('############################ {} L={} split={}'.format(dataset_name, L, split))
-print('N: {}, D: {}, Ns: {}'.format(X.shape[0], X.shape[1], Xs.shape[0]))
-
-Z = kmeans2(X, 100, minit='points')[0]
-
-D = X.shape[1]
-
-kernels = []
-for l in range(L):
-    kernels.append(RBF(D))
-
-for kernel in kernels[:-1]:
-    kernel += White(D, variance=2e-6)
-
-mb = minibatch_size if X.shape[0] > minibatch_size else None
-model = DGP(X, Y, Z, kernels, Gaussian(), num_samples=1, minibatch_size=mb)
-
-# start the inner layers almost deterministically
-for layer in model.layers[:-1]:
-    layer.q_sqrt = layer.q_sqrt.value * 1e-5
-model.likelihood.variance = 0.05
-
-global_step = tf.Variable(0, trainable=False, name="global_step")
-model.enquire_session().run(global_step.initializer)
-
-s = "{}/{}_L{}_split{}".format(results_path, dataset_name, L, split)
-fw = tf.summary.FileWriter(os.path.join(s.format(dataset_name, L)),
-                           model.enquire_session().graph)
-
-opt_method = gpflow_monitor.ManagedOptimisation(model, AdamOptimizer(0.01), global_step)
-
-its_to_print = (x * log_every for x in itertools.count())
-
-opt_method.tasks += [
-    gpflow_monitor.PrintTimings(its_to_print, gpflow_monitor.Trigger.ITER),
-    gpflow_monitor.ModelTensorBoard(its_to_print, gpflow_monitor.Trigger.ITER,
-                                    model, fw),
-    gpflow_monitor.LmlTensorBoard(its_to_print, gpflow_monitor.Trigger.ITER,
-                                  model, fw, verbose=False),
-    gpflow_monitor.StoreSession(its_to_print, gpflow_monitor.Trigger.ITER,
-                                model.enquire_session(), (s+'/checkpoints').format(dataset_name, L))
-]
-
-class TestTensorBoard(gpflow_monitor.ModelTensorBoard):
-    def __init__(self, sequence, trigger: gpflow_monitor.Trigger, model, file_writer, Xs, Ys):
-        super().__init__(sequence, trigger, model, file_writer)
-        self.Xs = Xs
-        self.Ys = Ys
-        self._full_test_err = tf.placeholder(tf.float64, shape=())
-        self._full_test_nlpp = tf.placeholder(tf.float64, shape=())
-
-        self.summary = tf.summary.merge([tf.summary.scalar("test_rmse", self._full_test_err),
-                                         tf.summary.scalar("test_nlpp", self._full_test_nlpp)])
-
-    def _event_handler(self, manager):
-        minibatch_size = 1000
-        S = 100
-        means, vars = [], []
-        for mb in range(-(-len(Xs) // minibatch_size)):
-            m, v = model.predict_y(Xs[mb * minibatch_size:(mb + 1) * minibatch_size, :], S)
-            means.append(m)
-            vars.append(v)
-        mean_SND = np.concatenate(means, 1)
-        var_SDN = np.concatenate(vars, 1)
-
-        mean_ND = np.average(mean_SND, 0)
-
-        test_err = np.average(Y_std * np.mean((Ys - mean_ND) ** 2.0) ** 0.5)
-        test_nll_ND = logsumexp(norm.logpdf(Ys * Y_std, mean_SND * Y_std, var_SDN ** 0.5 * Y_std), 0, b=1 / float(S))
-        test_nll = np.average(test_nll_ND)
-
-        summary, step = model.enquire_session().run([self.summary, global_step],
-                                                    feed_dict={self._full_test_err: test_err,
-                                                               self._full_test_nlpp: test_nll})
-        self.file_writer.add_summary(summary, step)
-
-
-
-opt_method.tasks.append(TestTensorBoard(its_to_print, gpflow_monitor.Trigger.ITER,
-                                        model, fw, Xs, Ys))
-
-
-
-
-opt_method.minimize(maxiter=iterations)
+if __name__ == "__main__":
+    main()
